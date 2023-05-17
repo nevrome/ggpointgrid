7930cf02c623301d931e4b2e0193da8adf6676d2
Clemens Schmid <clemens@nevrome.de>
2023-05-17 12:23:59 +0200

    iterative implementation with a better memory footprint

--- R/geom_pointgrid.R
+++ R/geom_pointgrid.R
@@ -133,7 +133,7 @@ arrange_points_on_grid <- function(tab, grid_x, grid_y) {
   )
   distance_long <- distance_long_raw
   # determine the optimal grid arrangement coordinates
-  grid_df <- core_arrange_algorithm3(distance_long)
+  grid_df <- core_arrange_algorithm4(distance_long)
   # replace input point coordinates with grid coordinates
   tab[grid_df$point_id, c("x", "y")] <- xy_grid[grid_df$grid_id,]
   return(tab)
@@ -146,9 +146,46 @@ arrange_points_on_grid <- function(tab, grid_x, grid_y) {
 # larger test
 # distance_matrix <- fields::rdist(expand.grid(1:100, 1:100), data.frame(x = sample(1:100, 4000, replace = T), y = sample(1:100, 4000, replace = T)))
 # distance_long <- stats::setNames(reshape2::melt(distance_matrix), c('grid_id', 'point_id', 'distance'))
+# core_arrange_algorithm4(distance_long)
 # core_arrange_algorithm3(distance_long)
 # core_arrange_algorithm2(distance_long)
 # core_arrange_algorithm(distance_long)
+
+core_arrange_algorithm4 <- function(distance_long) {
+  distance_long_sorted <- dplyr::arrange(distance_long, .data[["distance"]])
+  res_total <- list()
+  repeat {
+    res <- core_arrange_algorithm4in(distance_long_sorted)
+    res_total <- append(res_total, list(res$solved))
+    if (nrow(res$todo) > 0) {
+      distance_long_sorted <- res$todo
+    } else {
+      break
+    }
+  }
+  return(dplyr::bind_rows(res_total))
+}
+
+core_arrange_algorithm4in <- function(distance_long_sorted) {
+  # get smallest distance grid point for each input point
+  closest_grid_points <- distance_long_sorted[!duplicated(distance_long_sorted$point_id),]
+  # find grid point duplicates and create subsets with already uniquely claimed positions
+  grid_points_dups <- unique(closest_grid_points$grid_id[duplicated(closest_grid_points$grid_id)])
+  has_grid_point_dup <- closest_grid_points$grid_id %in% grid_points_dups
+  without_dups <- closest_grid_points[!has_grid_point_dup,]
+  with_dups <- closest_grid_points[has_grid_point_dup,]
+  # find best input point matches according to distance for duplicated grid points
+  with_dups_better <- with_dups[!duplicated(with_dups$grid_id),]
+  # combine newly solved point-grid pairs for the output grid
+  newly_solved <- dplyr::bind_rows(without_dups, with_dups_better)
+  # construct distance table with yet unattributed input and grid points
+  new_distance_long <- dplyr::anti_join(
+    dplyr::anti_join(distance_long_sorted, newly_solved, by = "grid_id"),
+    newly_solved, by = "point_id"
+  )
+  # output
+  return(list(solved = newly_solved, todo = new_distance_long))
+}
 
 core_arrange_algorithm3 <- function(distance_long, is_sorted = FALSE) {
   # sort table by distance if it's not already sorted
