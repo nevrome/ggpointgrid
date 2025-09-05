578ee01202e17c02232fb3be2bb89385465d7125
Clemens Schmid <nevromecs@gmail.com>
2025-09-05 13:46:34 +0200

    update of test code

--- playground/example.R
+++ playground/example.R
@@ -2,11 +2,12 @@
 
 library(ggplot2)
 
-n <- 2000
+n <- 3000
 my_data <- tibble::tibble(
   x = runif(n, -5, 5),
   y = runif(n, -5, 5),
-  value = sample(c("A", "B"), n, replace = T)
+  value = sample(c("A", "B"), n, replace = T),
+  id = 1:n
 )
 
 ggplot(my_data, aes(x = x, y = y, color = value)) +
@@ -17,23 +18,22 @@ ggplot(my_data, aes(x = x, y = y, color = value)) +
   ggpointgrid::geom_pointgrid(grid_x = 80, grid_y = 80) +
   coord_cartesian(xlim = c(-5, 5), ylim = c(-5, 5))
 
-axes <- ggpointgrid:::make_grid_axes_2D(my_data, grid_x = 50, grid_y = 50)
-system.time(arrange_points_on_grid_df(my_data, axis_x = axes[[1]], axis_y = axes[[2]]))
-system.time(arrange_points_on_grid_legacy(my_data, axis_x = axes[[1]], axis_y = axes[[2]]))
+axes <- ggpointgrid:::make_grid_axes_2D(my_data, grid_x = 80, grid_y = 80)
+system.time(res1 <- arrange_points_on_grid_df(my_data, axis_x = axes[[1]], axis_y = axes[[2]]))
+system.time(res2 <- arrange_points_on_grid_legacy(my_data, axis_x = axes[[1]], axis_y = axes[[2]]))
+apply(as.matrix(res1 != res2), 2, any)
 
-zu <- paste(
-  "echo",
+tu <- paste(
   "[", paste(axes[[1]], collapse = ","), "]",
   "[", paste(axes[[2]], collapse = ","), "]",
   "[", paste(my_data$x, collapse = ","), "]",
-  "[", paste(my_data$y, collapse = ","), "]",
-  "| ./src/arrange"
+  "[", paste(my_data$y, collapse = ","), "]"
 )
+zu <- paste("echo", tu, "| ./src/arrange")
 system(paste("time", zu))
 hu <- system(zu, intern = T)
 
-writeLines(zu, "futhark_test.txt")
-writeLines(hu[[1]] "futhark_test.txt")
+writeLines(tu, "futhark_test.txt")
 
 #### algorithm test ####
 
