d7fda6a9dbf95440a48883ccc15145f4fd02c72b
Clemens Schmid <nevromecs@gmail.com>
2026-05-15 13:40:09 +0200

    adjustments in the rcpp-c-futhark bridge, because futharks C API changed in v0.26.1: tuples are not longer implicitly unpacked

--- src/build.R
+++ src/build.R
@@ -1,5 +1,5 @@
-# download/update dependecies: system("cd src && futhark pkg sync")
-system("cd src && futhark c --library arrange.fut")
+# download/update dependencies: system("cd src && futhark pkg sync")
+system("cd src && futhark c --library entry.fut")
 #system("cd src && futhark multicore --library arrange.fut") # does not work on windows, unfortunately
 devtools::document()
 Rcpp::compileAttributes()
