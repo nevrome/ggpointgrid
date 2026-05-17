86df4a146aaca38eae732178f15fd3e503a0c06a
Clemens Schmid <clemens@nevrome.de>
2026-05-17 23:32:58 +0200

    found a bug in the sf polygon transformation

--- R/arrange_on_grid.R
+++ R/arrange_on_grid.R
@@ -217,7 +217,7 @@ as_futhark_polygon_format_sf <- function(geom) {
     } else if (g_type == "MULTIPOLYGON") {
       # g is list of polygons; each polygon is list of rings
       for (j in seq_along(g)) {
-        poly_rings <- purrr::map(g, function(ring) {as.matrix(ring)[, 1:2, drop = FALSE]})
+        poly_rings <- purrr::map(g[[j]], function(ring) {as.matrix(ring)[, 1:2, drop = FALSE]})
         all_polygons[[length(all_polygons) + 1]] <- poly_rings
       }
     }
